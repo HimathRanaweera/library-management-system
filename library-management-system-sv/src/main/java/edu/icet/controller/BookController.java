29c90166c2e33e96f779753eecaeca4ec3db8062
Himath Ranaweera <145730950+HimathRanaweera@users.noreply.github.com>
2024-02-28 11:23:50 +0530

    first commit

---
+++ library-management-system-sv/src/main/java/edu/icet/controller/BookController.java
@@ -0,0 +1,15 @@
+package edu.icet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@GetMapping("/book")
+
+public class BookController {
+@PostMapping("/add")
+    public void addBook() {
+
+}
+}
